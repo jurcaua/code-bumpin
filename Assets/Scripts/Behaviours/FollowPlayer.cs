21a854146e8bd078cba1636eb68b8c01820fc638
Alex <jurcaua@gmail.com>
2017-01-21 16:38:59 -0500

    working on pickup AND ITS GREEN NOW

---
+++ Assets/Scripts/Behaviours/FollowPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+	private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("MainCamera");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Follow();
+	}
+
+	void Follow() {
+		transform.rotation = Quaternion.LookRotation(player.transform.position - transform.position);
+	}
+}
---
+++ Assets/Scripts/Behaviours/Rotator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+	public float rotateSpeed = 50f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Rotate ();
+	}
+
+	void Rotate() {
+		transform.Rotate // unique spin is cool with those random value multipliers
+		(
+			rotateSpeed * Time.deltaTime * Random.value, 
+			rotateSpeed * Time.deltaTime * Random.value, 
+			rotateSpeed * Time.deltaTime * Random.value
+		);
+	}
+}
