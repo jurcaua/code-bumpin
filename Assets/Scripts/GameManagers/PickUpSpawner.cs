08bac53d2d7a485c2a3a3bb09648dc67df4bb3a3
Alex <jurcaua@gmail.com>
2017-01-21 17:34:17 -0500

    starting on pickup spawning lmao

---
+++ Assets/Scripts/GameManagers/PickUpSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpSpawner : MonoBehaviour {
+
+	public GameObject[] pickups;
+	public GameObject[] spawnPoints;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void SpawnPickup(){
+		int randPickupIndex = Random.Range (0, pickups.Length);
+		int randSpawnPointIndex = Random.Range (0, spawnPoints.Length);
+
+
+	}
+}
