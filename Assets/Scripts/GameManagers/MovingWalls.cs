df6d95997bd0d38970ef11fa523bd0f6580c5f67
Alex <jurcaua@gmail.com>
2017-01-21 15:44:30 -0500

    walls closing in, detecting game over is cool too

---
+++ Assets/Scripts/GameManagers/MovingWalls.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingWalls : MonoBehaviour {
+
+	public float minSpeed = 0.1f;
+
+	private GameObject[] walls;
+
+	// Use this for initialization
+	void Start () {
+		walls = new GameObject[transform.childCount];
+		for (int i = 0; i < walls.Length; i++) {
+			walls [i] = transform.GetChild (i).gameObject;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for (int i = 0; i < walls.Length; i++) {
+			walls [i].transform.position -=  walls [i].transform.position * minSpeed * AudioPeer.amplitude * Time.deltaTime;
+		}
+	}
+}
--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour {
 
+	private int wallsTouching = 0;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,10 +13,31 @@ public class PlayerManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		CheckSquished ();
 		CheckSides ();
 	}
 
+	// for wall jumping maybe i the future? that would be lit
 	void CheckSides(){
 
+	}
+
+	void CheckSquished(){
+		if (wallsTouching >= 3) {
+			Time.timeScale = 0; // for now just like pause this stuff i guess??? make an game over screen later
+			Debug.Log("GAME OVER");
+		}
+	}
+
+	void OnCollisionEnter (Collision collision){
+		if (collision.gameObject.tag == "Wall") {
+			wallsTouching += 1;
+		}
+	}
+
+	void OnCollisionExit (Collision collision){
+		if (collision.gameObject.tag == "Wall") {
+			wallsTouching -= 1;
+		}
 	}
 }
