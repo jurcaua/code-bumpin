3ea155058876f68fd6b7560c021c2486ead74324
Alex <jurcaua@gmail.com>
2017-01-22 06:06:09 -0500

    time UI added

--- Assets/Scripts/GameManagers/GameController.cs
+++ Assets/Scripts/GameManagers/GameController.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
 
+	public Text timeText;
+
 	private PickUpSpawner spawner;
+
+	private float currentTime = 0f;
 
 	// Use this for initialization
 	void Start () {
@@ -16,10 +21,32 @@ public class GameController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		currentTime += Time.deltaTime;
+		UpdateTime ();
+	}
+
+	void FixedUpdate(){
+
 	}
 
 	public void PlayButton(){
 		SceneManager.LoadScene ("main");
+	}
+
+	void UpdateTime(){
+		float minutes = Mathf.Round (currentTime / 60);
+		float seconds = Mathf.Round (currentTime % 60);
+
+		string minutesStr = minutes.ToString();
+		string secondsStr = seconds.ToString();
+
+		if (seconds % 10 == seconds) {
+			secondsStr = "0" + secondsStr;
+		}
+		if (minutes % 10 == minutes) {
+			minutesStr = "0" + minutesStr;
+		}
+
+		timeText.text = minutesStr + ":" + secondsStr;
 	}
 }
